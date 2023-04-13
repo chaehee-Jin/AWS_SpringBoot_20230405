cbfeca833a0266322999813575f936b1ea58a99e
chaehee-Jin <heeheer_g@naver.com>
2023-04-13 11:06:51 +0900

    db와 연동하여 회원가입 구현 중 / 비밀번호 암호화로 코드 구현

---
+++ src/main/java/com/web/study/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.web.study.service;
+
+import com.web.study.dto.request.auth.RegisteUserReqDto;
+
+public interface AuthService {
+	
+	public void registeUser(RegisteUserReqDto registeUserReqDto);
+	
+	
+	
+
+}
